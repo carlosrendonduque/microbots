9a537ac9a877faf5f7a507e459d6da60ce98ae9f
steven <stevenvallejo780@gmail.com>
2020-11-03 13:16:21 -0500

    fix(git)
    Arregle el bug del await y correji el manejo de modulos y git

---
+++ end_process/close_all_applications.js
@@ -0,0 +1,30 @@
+//Do the necessary procedures for ending the process (e.g., logout) and close the used applications.
+
+const CoreObj = require("../core_activities/core_activities");
+
+class close_all_applications {
+  constructor() {
+
+  }
+
+
+
+  async close_all_applications() {
+
+    try {
+
+      global.LOG_PROCESS['CLOSING_APPLICATIONS'] = process.env.LOGMESSAGE_CLOSING_APPLICATIONS;
+      //TODO: Code to close the used applications.
+      const result = await CoreObj.save_log("Trace", global.LOG_PROCESS['CLOSING_APPLICATIONS']);
+      return result;
+    }
+    catch (e) {
+
+    }
+    finally {
+
+    }
+  }
+}
+
+module.exports = new close_all_applications();
